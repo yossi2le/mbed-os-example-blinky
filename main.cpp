50ae9f7225f88350da2f0d97cad0f583b26b4aa8
Yossi Levy <Yossi.Levy@arm.com>
2019-02-19 22:12:37 +0200

    blinky with support of bootloader for CY8CKIT_062_WIFI_BT_PSA and CY8CKIT_062_WIFI_BLE

--- main.cpp
+++ main.cpp
@@ -14,19 +14,21 @@ DigitalOut led1(LED1);
 // main() runs in its own thread in the OS
 int main()
 {
-    SystemReport sys_state( SLEEP_TIME * PRINT_AFTER_N_LOOPS /* Loop delay time in ms */);
-
     int count = 0;
     while (true) {
         // Blink LED and wait 0.5 seconds
         led1 = !led1;
-        wait_ms(SLEEP_TIME);
+        //wait_ms(SLEEP_TIME);
+        printf("I am alive\r\n");
+        for (volatile unsigned int i = 0; i < 10000000; i++) {
 
-        if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
-            // Following the main thread wait, report on the current system status
-            sys_state.report_state();
-            count = 0;
         }
-        ++count;
+
+//        if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
+//            // Following the main thread wait, report on the current system status
+//            sys_state.report_state();
+//            count = 0;
+//        }
+//        ++count;
     }
 }
